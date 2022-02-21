d14e00107afbe179c239eea7187c16a6bd52bacb
Шухман Вероника <nikshuhman@gmail.com>
2022-02-21 12:02:34 +0200

    add func interactive

--- lab1.go
+++ lab1.go
@@ -1,9 +1,12 @@
 package main
 
 import (
- 
+	"fmt"
+  "os"
 	"math"
-
+	"bufio"
+	"strconv"
+	"strings"
 )
 func main(){
 
@@ -30,3 +33,37 @@ func solutionEquation (param []float64) []float64 {
 	return roots
 }
 
+func interactive () []float64{
+
+	var param []float64
+	var arguments = []string{"a", "b", "c"} 
+ 
+	var readInput = bufio.NewReader(os.Stdin)
+ 
+	for  i := 0; i != 3; i++{
+	 fmt.Printf("%s: ", arguments[i])
+	 var parameters, flaw  = readInput.ReadString('\n')
+	 if flaw != nil {
+		fmt.Println("Произошла ошибка при вводе данных. Повторите попытку", flaw)
+		continue
+	 }
+ 
+	 parameters = strings.TrimSuffix(parameters, "\r\n")
+	 parameters1, flaw := strconv.ParseFloat(parameters, 64)
+	 if flaw != nil {
+		fmt.Printf("Ошибка. Введите правильные значения пж, получили %s \n", parameters)
+		continue
+	 }
+ 
+	 if parameters1 == 0 && len(param) == 0 {
+		fmt.Print("Ошибка. а не может быть 0\n")
+		continue
+	 }
+	 param = append(param, parameters1)
+	}
+ 
+	return param
+ 
+ }
+ 
+ 
